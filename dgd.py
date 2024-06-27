b1f22cdae0d9ce5829db1b026027442117277ffa
MINNAKOV <minakov.nikita.1997@mail.ru>
2024-06-27 16:04:43 +0300

    Add files via upload

---
+++ dgd.py
@@ -0,0 +1,9 @@
+homework_done_count = 12
+spent_time = 1.5
+name_of_course = "Phyton"
+time_of_one_task = (spent_time/homework_done_count)
+a = homework_done_count
+b = spent_time
+c = name_of_course
+d = time_of_one_task
+print("Курс",":",c,"," "всего задач",":",a,','"затрачено часов",":",b,',',"среднее выполнения задания"' ',d,'.',sep='')
